8dc14f5d467b386708e713aefc2cc043a7d81b02
kazumajj <1990342976@qq.com>
2026-04-06 22:16:15 +0800

    通知

--- app/message.tsx
+++ app/message.tsx
@@ -3,8 +3,22 @@ import { View, StyleSheet, ScrollView, Pressable, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Arrowback from "../assets/images/arrow-back.svg";
 import MessageTip from "@/components/messageTip";
+import { use, useEffect,useState } from "react";
+import { getNotificationslist } from "./api/me";
+import { notfiItem } from "./api/interface";
+
 export default function Message() {
   const router = useRouter();
+  const [notifications, setNotifications] = useState<notfiItem[]>([]);
+  const [next_cursor, setNext_cursor]=useState("")
+  useEffect(()=>{
+    const getNotifications=async()=>{
+      const res=await getNotificationslist()
+      setNotifications(res.data.items)
+      setNext_cursor(res.data.next_cursor)
+    }
+    getNotifications()
+  },[])
   return (
     <SafeAreaProvider style={styles.container}>
       <View style={styles.header}>
@@ -18,20 +32,13 @@ export default function Message() {
         </Pressable>
       </View>
       <ScrollView style={styles.messagelist}>
-        <MessageTip/>
-         <MessageTip/>
-        <MessageTip/>
-        <MessageTip/>
-        <MessageTip/>
-        <MessageTip/>
-        <MessageTip/>
-        <MessageTip/>
-        <MessageTip/>
-        <MessageTip/>
-        <MessageTip/>
-        <MessageTip/>
-        <MessageTip/>
-        <MessageTip/>
+        {
+          notifications && notifications.map((item,index)=>{
+            return(
+              <MessageTip key={index} {...item} />
+            )
+          })
+        }
        
       </ScrollView>
     </SafeAreaProvider>
