d23865fd6256e6259ac548a201262e0b4d5660e5
kazumajj <1990342976@qq.com>
2026-04-15 22:58:48 +0800

    设置Image尺寸

--- components/messageTip.tsx
+++ components/messageTip.tsx
@@ -1,29 +1,40 @@
 import { notfiItem } from "@/app/api/interface";
-import { View, StyleSheet, ImageBackground, Text } from "react-native";
+import { View, StyleSheet, ImageBackground, Text, Image } from "react-native";
 import Inspire from "../assets/images/inspiration.svg";
 import Empathy from "../assets/images/resonance.svg";
+import { use, useEffect } from "react";
 export default function MessageTip(props: notfiItem) {
   const formatDate = (dateStr: string) => {
     const date = new Date(dateStr);
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
-     const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
     return `${year}/${month}/${day} ${hours}:${minutes}`;
   };
   return (
     <View style={styles.tips}>
       <View style={styles.touxiang}>
-        <ImageBackground source={{ uri: props.actor_avatar_url }} />
+        <ImageBackground
+          source={{ uri: props.actor_avatar_url }}
+          style={{
+            width: 40,
+            height: 40,
+            borderRadius: 20,
+            overflow: "hidden",
+          }}
+        />
         {props.reaction_type === "inspired" ? (
           <Inspire style={styles.Icon} />
         ) : (
-          <Empathy style={styles.Icon}/>
+          <Empathy style={styles.Icon} />
         )}
       </View>
       <View>
-        <Text style={{ color: "#999", fontSize: 12 }}>{formatDate(props.created_at)}</Text>
+        <Text style={{ color: "#999", fontSize: 12 }}>
+          {formatDate(props.created_at)}
+        </Text>
         {props.reaction_type === "inspired" ? (
           <Text style={styles.Inpiretext}>受到了启发</Text>
         ) : (
@@ -32,9 +43,10 @@ export default function MessageTip(props: notfiItem) {
       </View>
       <View style={styles.zuopin}>
         {props.cover_image && (
-          <ImageBackground
+          <Image
             source={{ uri: props.cover_image.square_small.url }}
-          ></ImageBackground>
+            style={{ width: "100%", height: "100%"}}
+          ></Image>
         )}
       </View>
     </View>
@@ -62,9 +74,9 @@ const styles = StyleSheet.create({
   },
   Icon: {
     position: "absolute",
-    top:26,
-    left:26,
-    zIndex:1
+    top: 26,
+    left: 26,
+    zIndex: 1,
   },
   Inpiretext: {
     color: "#666666",
